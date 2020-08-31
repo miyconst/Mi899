e0f1859f306b5fd85896e09068035718d9746b0b
Konstantin <const.miyang@gmail.com>
2020-08-31 14:38:17 +0200

    WIP: add some model classes and some data

---
+++ Mi899.Data/Bios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mi899.Data
+{
+    internal class Bios : IBios
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string[] MotherboardIds { get; set; }
+        public string Description { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
---
+++ Mi899.Data/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mi899.Data
+{
+    public class Model
+    {
+        private List<Motherboard> _motherboards;
+        private List<Bios> _bioses;
+
+        public IReadOnlyList<IMotherboard> Motherboards => _motherboards;
+        public IReadOnlyList<IBios> Bioses => _bioses;
+
+        public Model()
+        {
+            _motherboards = new List<Motherboard>();
+            _bioses = new List<Bios>();
+        }
+    }
+}
