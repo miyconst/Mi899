b2b1839865ba9cba7b20dbe7cff9b3ee31a26fdc
Kostiantyn Cherniavskyi <const.miyang@gmail.com>
2026-01-07 19:45:00 +0100

    Mi899 Online into `docs` for gh-pages

--- src/Mi899.Online/Program.cs
+++ src/Mi899.Online/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseStaticFiles();
+StaticFileOptions options = new()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "../../docs"))
+};
+app.UseStaticFiles(options);
 
 app.Run();
